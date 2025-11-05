29c9b0e5c38eeea7d0f7ffa43b657a4c04a87405
vgmda <vasilegancin@gmail.com>
2025-11-05 11:16:48 +0000

    Comments

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
+// Output for private cluster connection string
 output "private_srv_connection_string" {  
   value = module.atlas_private_ips.private_srv_connection_string  
 }  
-  
+
+// Output for private shard IPs
 output "private_shard_ips" {  
   value = module.atlas_private_ips.private_shard_ips  
 }  
